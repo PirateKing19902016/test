5a0a604034bc7f8514ff628d3e0a5c5b6c481ed9
PirateKing19902016 <nikhilsharma19902016@gmail.com>
2020-02-14 23:23:24 +0530

    workkkkkkkk

--- tests/testthat/test-CRAN-create-track-hub.R
+++ tests/testthat/test-CRAN-create-track-hub.R
@@ -3,6 +3,7 @@ library(testthat)
 library(PeakSegPipeline)
 
 chrom.sizes.file <- tempfile(tmpdir = tempdir())
+tempdir(check = FALSE)
 chrom.sizes <- data.table(chrom="chr10", bases=128616069)
 fwrite(chrom.sizes, chrom.sizes.file, sep="\t", col.names=FALSE)
 print(chrom.sizes.file)
